5f28a277cf3ca3fe6da2e3578d43d72d335fcd5e
bjelicamarko <marko.bjelica19@gmail.com>
2022-01-12 18:01:55 +0100

    Added function.

--- restaurant-front/src/modules/reports/components/bar-chart/bar-chart.component.ts
+++ restaurant-front/src/modules/reports/components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -36,12 +36,8 @@ export class BarChartComponent {
      DataLabelsPlugin
   ];
 
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
-    datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
-    ]
+  @Input() barChartData: ChartData<'bar'> = {
+    datasets: []
   };
 
   // events
--- restaurant-front/src/modules/reports/pages/sales-chart/sales-chart.component.ts
+++ restaurant-front/src/modules/reports/pages/sales-chart/sales-chart.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ChartData } from 'chart.js';
+import { Sales } from '../../models/sales';
+import { ReportsService } from '../../services/reports.service';
+import { SharedDatePickerService } from '../../services/shared-date-picker.service';
 
 @Component({
   selector: 'app-sales-chart',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-chart.component.scss']
 })
 export class SalesChartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  barChartData: ChartData<'bar'> = {
+    datasets: [] 
+  };
+  salesList: Sales[]  = [] ;
+  
+  public range = new FormGroup({});
+  
+  constructor(private reportsService: ReportsService, private sharedDatePickerService: SharedDatePickerService) { 
+    this.sharedDatePickerService.getData()
+      .subscribe(res => this.range = res);
   }
 
+  ngOnInit(): void {
+    this.reportsService.getSalesTest()
+        .subscribe(response => {
+          this.salesList = response.body as Sales[];
+          this.fillBar()
+        });
+  }
+
+  private fillBar(): void {
+    if (this.salesList.length  === 0) {
+      return;
+    }
+
+    this.barChartData.datasets = [];
+    this.barChartData.labels = [];
+    
+    for (let i = 0; i < this.salesList.length; i++ ) {
+      this.barChartData.datasets.push({data:[]});
+      let counter = 0;
+      
+      for (const [key, value] of this.salesList[i].salesPerMonth.entries()) {
+        console.log(key, value);
+        if (i === 0) {
+          this.barChartData.labels.push(key);
+        }
+        this.barChartData.datasets[counter].data.push(value.priceCount);
+        ++counter;
+      }
+    
+      
+    }
+    
+    
+
+  }
 }
