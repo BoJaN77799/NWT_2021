9de70238ee07e01a1aada11b9b9bc32a29a2593b
bjelicamarko <marko.bjelica19@gmail.com>
2022-01-14 14:20:49 +0100

    little modification

--- restaurant-front/src/modules/employees/employees.module.ts
+++ restaurant-front/src/modules/employees/employees.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    //RouterModule.forChild()
   ]
 })
 export class EmployeesModule { }
