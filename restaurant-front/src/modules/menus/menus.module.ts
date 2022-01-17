cb6836cc7076343be37b49d2c1165e2d6bb694de
bjelicamarko <marko.bjelica19@gmail.com>
2022-01-17 15:21:46 +0100

    initial commit

---
+++ restaurant-front/src/modules/menus/menus.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenusManagerComponent } from './pages/menus-manager/menus-manager.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenusManagerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MenusModule { }
---
+++ restaurant-front/src/modules/menus/pages/menus-manager/menus-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menus-manager',
+  templateUrl: './menus-manager.component.html',
+  styleUrls: ['./menus-manager.component.scss']
+})
+export class MenusManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
