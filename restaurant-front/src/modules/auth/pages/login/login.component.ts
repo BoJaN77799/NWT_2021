56618320a4266a82d22027b65b01eb261689ba4a
Bojan <bojanbaskalo@hotmail.rs>
2022-01-27 13:12:51 +0100

    login-page css refactored

--- restaurant-front/src/modules/auth/pages/login/login.component.ts
+++ restaurant-front/src/modules/auth/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from "@angular/router";
 import { JwtHelperService } from "@auth0/angular-jwt";
 import { ToastrService } from "ngx-toastr";
 import { Login } from "src/modules/shared/models/login";
+import { SnackBarService } from "src/modules/shared/services/snack-bar.service";
 import { AuthService } from "../../services/auth-service/auth.service";
 
 @Component({
@@ -11,14 +12,14 @@ import { AuthService } from "../../services/auth-service/auth.service";
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private toastr: ToastrService
+    private snackBarService: SnackBarService
   ) {
     this.form = this.fb.group({
       email: [null, Validators.required],
@@ -26,47 +27,42 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
-
   submit() {
     const auth: Login = {
       email: this.form.value.email,
       password: this.form.value.password,
     };
 
-    this.authService.login(auth).subscribe(
-      (result) => {
-        this.toastr.success("Successful login!");
+    this.authService.login(auth).subscribe((result) => {
+      this.snackBarService.openSnackBar("Successful login!");
 
-        const token = JSON.stringify(result);
-        localStorage.setItem("user", token);
+      const token = JSON.stringify(result);
+      localStorage.setItem("user", token);
 
-        const jwt: JwtHelperService = new JwtHelperService();
-        const info = jwt.decodeToken(token);
-        const role = info.role;
-        if (role === "ADMINISTRATOR") {
-          this.router.navigate(["rest-app/tables/tables-admin"]);
-        }
-        else if (role === "MANAGER") {
-          this.router.navigate(["rest-app/employees/employees-manager"]);
-        }
-        else if (role === "COOK") {
-          this.router.navigate(["rest-app/orders/orders-page"]);
-        }
-        else if (role === "HEAD_COOK") {
-          this.router.navigate(["rest-app/orders/orders-page"]);
-        }
-        else if (role === "BARMAN") {
-          this.router.navigate(["rest-app/orders/orders-page"]); // mozda nece biti redirektovan ovdje... Ako bude spojicu
-        }
-        else if (role === "WAITER") {
-          this.router.navigate(["rest-app/orders/create-order-page/0"]);
-        }
-        //TODO ovde dodati if za svakog korisnika, i poslati ga na home page sa 
-        //         this.router.navigate(["putanja_do_home_page"]);
-      },
-      (error) => {
-        this.toastr.error(error.error);
+      const jwt: JwtHelperService = new JwtHelperService();
+      const info = jwt.decodeToken(token);
+      const role = info.role;
+      if (role === "ADMINISTRATOR") {
+        this.router.navigate(["rest-app/tables/tables-admin"]);
+      }
+      else if (role === "MANAGER") {
+        this.router.navigate(["rest-app/employees/employees-manager"]);
+      }
+      else if (role === "COOK") {
+        this.router.navigate(["rest-app/orders/orders-page"]);
+      }
+      else if (role === "HEAD_COOK") {
+        this.router.navigate(["rest-app/orders/orders-page"]);
+      }
+      else if (role === "BARMAN") {
+        this.router.navigate(["rest-app/orders/orders-page"]);
+      }
+      else if (role === "WAITER") {
+        this.router.navigate(["rest-app/orders/create-order-page/0"]);
+      }
+    },
+      (err) => {
+        this.snackBarService.openSnackBar(err.error);
       }
     );
   }
